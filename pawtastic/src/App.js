93069fb7ea67ab6131281adf79e9a9340952d46b
Student_GA113 <Student_GA113@Student-GA113.local>
2020-03-27 15:04:56 -0400

    add Name,Breed,Birthday forms

--- pawtastic/src/App.js
+++ pawtastic/src/App.js
@@ -1,16 +1,54 @@
-import React from 'react';
 import './App.css';
+import React, { Component } from 'react'
 
-class App extends React.Component{
-  // function App() {
-    render(){
-      return (
-        <div className="App">
-          <h1>Hello World</h1>
-        </div>
-      );
-    }
-  
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {value: ''}
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    alert(`A name was submitted: ${this.state.value}`);
+    event.preventDefault();
+
+  } 
+
+  render() {
+    return (
+      <div>
+<form onSubmit={this.handleChange}>
+        <label>  
+          Name: 
+          <input type="text"  value={this.state.value} onChange={this.handleChange}/>
+          <input type="submit"  value="Submit" onClick={this.handleSubmit}/>
+        </label>
+      </form>
+      <form onSubmit={this.handleChange}>
+      <label>  
+          Breed: 
+          <input type="text"  value={this.state.value} onChange={this.handleChange}/>
+          <input type="submit"  value="Submit" onClick={this.handleSubmit}/>
+        </label>
+      </form>
+      <form onSubmit={this.handleChange}>
+      <label>  
+          Birthday: 
+          <input type="date"  value={this.state.value} onChange={this.handleChange}/>
+          <input type="submit"  value="Submit" onClick={this.handleSubmit}/>
+        </label>
+      </form>
+      </div>
+      
+    )
+  }
 }
 
-export default App;
