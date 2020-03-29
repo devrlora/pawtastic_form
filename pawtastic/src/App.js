13df93635ba905c8624a2a75326879f34d1e00de
Student_GA113 <Student_GA113@Student-GA113.local>
2020-03-28 22:22:41 -0400

    add psuedocode for component structure

--- pawtastic/src/App.js
+++ pawtastic/src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import React, { Component } from 'react'
-import DogName from './components/DogName'
-import DogBreed from './components/DodBreed'
-import DogDob from './components/DogDob';
+// import { DogName } from './components/DogName';
+// import { DogBreed } from './components/DogBreed';
+// import { DogDob } from './components/DogName';
 
 
+          {/* Create a component class */}
 export default class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {value: ''}
 
+          {/* create a way to pass user input to to other components */}
+
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+            {/* create a way to register an event change */}
 
   handleChange(event) {
     this.setState({value: event.target.value})
@@ -28,26 +33,35 @@ export default class App extends Component {
   render() {
     return (
       <div>
-
         <img src="https://i.dlpng.com/static/png/6103810-dog-paw-paw-prints-transparent-background-png-clipart-hiclipart-black-paw-print-png-800_750_preview.png"alt="paw logo"/>
-        
-        {/* Welcome text */}
-        <h1>Yay, we love dogs! Give Us the basics about your pup.</h1>  
-        <div>
-           <DogName>
-           <DogBreed>
-           <DogDob>
-      <div>
-      <input type="submit" value="Submit" />
-      </div>
-      
-      
+        <h1>Yay, we love dogs! Give Us the basics about your pup.</h1>
+         <form onSubmit={this.handleChange}>
+           <div>
            </div>
-       
-     
+
+        {/* create a component for each label, 
+        use component instance in App.js div instead of HTML elements*/}
+
+        <label>  
+          Name: 
+          <input type="text"  value={this.state.value} onChange={this.handleChange}/>
+        </label>
+      </form>
+      <form onSubmit={this.handleChange}>
+      <label>  
+          Breed: 
+          <input type="text"  value={this.state.value} onChange={this.handleChange}/>
+        </label>
+      </form>
+      <form onSubmit={this.handleChange}>
+      <label>  
+          Birthday: 
+          <input type="date"  value={this.state.value} onChange={this.handleChange}/>
+          {/* <input type="submit"  value="Submit" onClick={this.handleSubmit}/> */}
+        </label>
+      </form>
       </div>
-      
-    )
+    );
   }
 }
 
