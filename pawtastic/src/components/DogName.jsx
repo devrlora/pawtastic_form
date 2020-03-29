316e565ec8189bce1824db23e5e6758781adf226
Student_GA113 <Student_GA113@Student-GA113.local>
2020-03-28 21:47:19 -0400

    add component folder

---
+++ pawtastic/src/components/DogName.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {value: ''}
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    alert(`A name was submitted: ${this.state.value}`);
+    event.preventDefault();
+
+  } 
+
+  render() {
+    return (
+      <div>
+
+        <img src="https://i.dlpng.com/static/png/6103810-dog-paw-paw-prints-transparent-background-png-clipart-hiclipart-black-paw-print-png-800_750_preview.png"alt="paw logo"/>
+        
+        {/* Welcome text */}
+        <h1>Yay, we love dogs! Give Us the basics about your pup.</h1>
+         
+           <div>
+           <form onSubmit={this.handleChange}>
+           <label>  
+          Name: 
+          <input type="text"  value={this.state.value} onChange={this.handleChange}/>
+        </label>
+      </form>
+      <form onSubmit={this.handleChange}>
+      <label>  
+          Breed: 
+          <input type="text"  value={this.state.value} onChange={this.handleChange}/>
+          {/* <input type="submit"  value="Submit" onClick={this.handleSubmit}/> */}
+        </label>
+      </form>
+      <form onSubmit={this.handleChange}>
+      <label>  
+          Birthday: 
+          <input type="date"  value={this.state.value} onChange={this.handleChange}/>
+          {/* <input type="submit"  value="Submit" onClick={this.handleSubmit}/> */}
+        </label>
+        
+      </form>
+      <div>
+      <input type="submit" value="Submit" />
+      </div>
