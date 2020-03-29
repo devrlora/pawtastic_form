4a7ad1e55b83ed1fc94bba69cae73470ea271d8c
Student_GA113 <Student_GA113@Student-GA113.local>
2020-03-28 22:24:06 -0400

    remove copied text

--- pawtastic/src/components/DodBreed.jsx
+++ pawtastic/src/components/DodBreed.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-<label>  
-          Breed: 
-          <input type="text"  value={this.state.value} onChange={this.handleChange}/>
-          {/* <input type="submit"  value="Submit" onClick={this.handleSubmit}/> */}
-        </label>
 
       
-export default DogBreed;
+// export default DogBreed;
