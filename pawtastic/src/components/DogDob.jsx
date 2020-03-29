381e3bfa62b269786135c7b4c57fa4814f7bb090
Student_GA113 <Student_GA113@Student-GA113.local>
2020-03-28 22:03:42 -0400

    create a class component for input

---
+++ pawtastic/src/components/DogDob.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+<label>  
+          Birthday: 
+          <input type="date"  value={this.state.value} onChange={this.handleChange}/>
+          {/* <input type="submit"  value="Submit" onClick={this.handleSubmit}/> */}
+        </label>
+
+
+export default DogDob;
