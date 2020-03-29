063f1e84f2900498b1f2dcf7535f3fc2203ca822
Student_GA113 <Student_GA113@Student-GA113.local>
2020-03-28 22:03:04 -0400

    create a class component for input

---
+++ pawtastic/src/components/DodBreed.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+<label>  
+          Breed: 
+          <input type="text"  value={this.state.value} onChange={this.handleChange}/>
+          {/* <input type="submit"  value="Submit" onClick={this.handleSubmit}/> */}
+        </label>
+
+      
+export default DogBreed;
